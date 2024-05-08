55e861aaa2414df1959586dd2024399fb9004937
Akash Khan <akashcc48@gmail.com>
2024-05-08 08:19:20 +0600

    cine rental project added

---
+++ src/cine/Rating.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import Star from '../assets/star.svg'
+
+const Rating = ({value}) => {
+   const stars =  Array(value).fill(Star)
+    return(
+        <>
+             {
+                stars.map((star,index)=> (
+                    <img key={index} src={star} alt="star" width={14} height={14} />
+                ))
+             }
+        </>
+    )
+}
+
+export default Rating;
